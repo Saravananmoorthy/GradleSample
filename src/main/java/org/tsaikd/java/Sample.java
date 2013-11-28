1c0369b278af00122f78d9e5e89b59f965772414
tsaikd <tsaikd@gmail.com>
2013-11-28 13:41:15 +0800

    init

---
+++ src/main/java/org/tsaikd/java/Sample.java
@@ -0,0 +1,14 @@
+package org.tsaikd.java;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Sample {
+
+	static Log log = LogFactory.getLog(Sample.class);
+
+	public static void main(String[] args) {
+		log.debug("Hello");
+	}
+
+}
